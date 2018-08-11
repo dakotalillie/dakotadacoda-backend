1037e9d2b24be856744c174e17b1983dbfe13300
Dakota Lillie <dakota.lillie@icloud.com>
2018-08-11 13:11:02 -0400

    incorporate Joi

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,27 +1,42 @@
 'use strict';
 
 const config = require('config');
+const Joi = require('joi');
 
 const createVerifiedTransport = require('../setup/setupEmail');
 
 async function sendEmail(req, res) {
+  const { error } = validateParams(req);
+  if (error) return res.status(400).send({ error: error.details[0].message} );
+
   const { name, email, subject, text } = req.body;
   const myEmail = config.get('email');
 
   const transporter = await createVerifiedTransport();
 
   try {
-    await transporter.sendMail({
-      from: myEmail,
-      to: myEmail,
-      replyTo: email,
-      subject,
-      text: `You have a new email from ${name}:\n\n${text}`
-    });
-    res.send();
+    // await transporter.sendMail({
+    //   from: myEmail,
+    //   to: myEmail,
+    //   replyTo: email,
+    //   subject,
+    //   text: `You have a new email from ${name}:\n\n${text}`
+    // });
+    res.send(req.body);
   } catch (err) {
     res.status(500).send(err.message);
   }
+}
+
+function validateParams(req) {
+  const schema = {
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    subject: Joi.string().required(),
+    text: Joi.string().required()
+  };
+
+  return Joi.validate(req.body, schema);
 }
 
 module.exports = {
--- tests/integration/routes.test.js
+++ tests/integration/routes.test.js
@@ -2,17 +2,34 @@ const request = require('supertest');
 
 describe('POST /api/v1', () => {
 
-  let server;
+  let server, params;
   beforeEach(async () => {
-    server = require('../../app').server;
+    server = require('../../app');
+    params =  {
+      name: 'My Name',
+      email: 'my@email.com',
+      subject: 'Test Subject',
+      text: 'Test Text'
+    };
   });
 
   afterEach(() => {
     server.close();
   });
 
+  it('should respond 400 if given invalid params', async () => {
+    const requiredParams = ['name', 'email', 'subject', 'text'];
+    for (const param of requiredParams) {
+      const tmp = params[param];
+      params[param] = '';
+      const res = await request(server).post('/api/v1').send(params);
+      expect(res.status).toBe(400);
+      params[param] = tmp;
+    }
+  });
+
   it('should give a response code of 200', async () => {
-    const res = await request(server).post('/api/v1');
+    const res = await request(server).post('/api/v1').send(params);
     expect(res.status).toBe(200);
   });
 });
